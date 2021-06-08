1f922722419bc8f72b5c516e96cd1bf4af379e9c
Rohit kumar <rohitjadav1729@gmail.com>
2021-06-08 12:53:37 +0530

    initial commit

---
+++ build/precache-manifest.0f5ae9845c69cc234663f005759b4fdb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01c4cb352bdba7622d3f20ffc9f924c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3fa5ac507983a3b476d",
+    "url": "/static/js/2.b6ca354c.chunk.js"
+  },
+  {
+    "revision": "0749b9159a8f03f9860d",
+    "url": "/static/js/main.c20aba3b.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
